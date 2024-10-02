9d8d083e5f85e0fa17d854b2a9e380e9aedfb16b
Valentin <valentin.geras@yandex.ru>
2024-10-02 22:15:31 +0300

    Создание проекта

---
+++ src/main/java/ru/custis/course_selection/CourseSelectionApplication.java
@@ -0,0 +1,13 @@
+package ru.custis.course_selection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSelectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSelectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/custis/course_selection/CourseSelectionApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.custis.course_selection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSelectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
